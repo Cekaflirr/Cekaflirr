2438ec012418fef794bc726039f2801debcdc859
Cekaflirr <86376140+Cekaflirr@users.noreply.github.com>
2022-09-14 14:59:41 +0200

    Add files via upload

---
+++ Accueil.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projet A</title>
+
+</head>
+<body>
+    <body style="background-color:#0E5F9A">
+</body>
+<header>
+</header>
+</html>
